ce950d44f92383d765108ac5cf8848ba63333bcf
unknown <saurabhdudhalwar@gmailcom>
2024-10-16 16:18:05 +0530

    Custom Delimiters Supports Test Case

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -24,3 +24,8 @@ test("should return the sum of multiple numbers", () => {
 test("should handle newlines between numbers", () => {
   expect(add("1\n2,3")).toBe(6);
 });
+
+// custom delimiter supports test case
+test("should support custom delimiters", () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
