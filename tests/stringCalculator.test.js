bb6d646563045489234f741a1ea07a83fdd6b5c6
unknown <saurabhdudhalwar@gmailcom>
2024-10-16 16:06:28 +0530

    Single Input Test Case

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,5 +1,11 @@
-const add = require('../src/stringCalculator');
+const add = require("../src/stringCalculator");
 
-test('should return 0 when input is an empty string', () => {
+// empty string test case
+test("should return 0 when input is an empty string", () => {
   expect(add("")).toBe(0);
+});
+
+// single input test case
+test("should return the number when a single number is passed", () => {
+  expect(add("1")).toBe(1);
 });
